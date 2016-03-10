0d719d66c7e493e3c72b619cb48c5163243e966f
Szczepan Faber <szczepiq@gmail.com>
2016-03-09 20:54:46 -0800

    Removed coupling with java plugin
    We don't want the test to break every time java plugin changes.

--- src/test/groovy/com/getcraftdone/gradle/internal/PluginDocGeneratorTest.groovy
+++ src/test/groovy/com/getcraftdone/gradle/internal/PluginDocGeneratorTest.groovy
@@ -13,69 +13,13 @@ class PluginDocGeneratorTest extends Specification {
   def project = new ProjectBuilder().build()
   @Rule TemporaryFolder tmp = new TemporaryFolder()
 
-  def "generates doc"() {
+  def "generates doc for java plugin"() {
     def f = tmp.newFile()
 
     when:
     new PluginDocGenerator().textDoc(project, "java", f)
 
-    then:
-    f.text == """ --- Documentation for 'java' plugin ---
-
-  * Plugin: LifecycleBasePlugin
-    Tasks: none
-    Configurations: none
-
-
-  * Plugin: BasePlugin
-    Tasks:
-     - assemble - Assembles the outputs of this project.
-    Configurations:
-     - archives - Configuration for archive artifacts.
-     - default - Configuration for default artifacts.
-
-
-  * Plugin: ReportingBasePlugin
-    Tasks: none
-    Configurations: none
-
-
-  * Plugin: LanguageBasePlugin
-    Tasks: none
-    Configurations: none
-
-
-  * Plugin: BinaryBasePlugin
-    Tasks: none
-    Configurations: none
-
-
-  * Plugin: JavaBasePlugin
-    Tasks:
-     - buildDependents - Assembles and tests this project and all projects that depend on it.
-     - buildNeeded - Assembles and tests this project and all projects it depends on.
-    Configurations: none
-
-
-  * Plugin: JavaPlugin
-    Tasks:
-     - check - Runs all checks.
-     - classes - Assembles main classes.
-     - compileJava - Compiles main Java source.
-     - compileTestJava - Compiles test Java source.
-     - jar - Assembles a jar archive containing the main classes.
-     - javadoc - Generates Javadoc API documentation for the main source code.
-     - processResources - Processes main resources.
-     - processTestResources - Processes test resources.
-     - test - Runs the unit tests.
-     - testClasses - Assembles test classes.
-    Configurations:
-     - compile - Compile classpath for source set 'main'.
-     - runtime - Runtime classpath for source set 'main'.
-     - testCompile - Compile classpath for source set 'test'.
-     - testRuntime - Runtime classpath for source set 'test'.
-
-
-"""
+    then: //sanity test only
+    f.text
   }
 }
