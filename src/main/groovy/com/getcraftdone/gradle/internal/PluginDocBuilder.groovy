a22050f8a0c254cc770073d3cbc36ce8d94126c3
Szczepan Faber <szczepiq@gmail.com>
2016-03-09 20:52:29 -0800

    Removed debug statements

--- src/main/groovy/com/getcraftdone/gradle/internal/PluginDocBuilder.groovy
+++ src/main/groovy/com/getcraftdone/gradle/internal/PluginDocBuilder.groovy
@@ -7,7 +7,7 @@ import com.getcraftdone.gradle.internal.aop.PluginLifecycleAspect
 /**
  * Created by sfaber on 3/5/16.
  */
-class PluginDocBuilder implements PluginLifecycleAspect.Listener{
+class PluginDocBuilder implements PluginLifecycleAspect.Listener {
 
   ModelBuilder builder = new ModelBuilder()
   Project project
@@ -24,12 +24,10 @@ class PluginDocBuilder implements PluginLifecycleAspect.Listener{
   LinkedList<ModelSnapshot> snapshots = new LinkedList<>()
 
   void beforeApplied(Plugin<Project> plugin) {
-    println "Before $plugin"
     snapshots << new SnapshotTaker().snapshot(project, plugin)
   }
 
   void afterApplied(Plugin<Project> plugin) {
-    println "After $plugin"
     def s = new SnapshotTaker().snapshot(project, plugin)
     def delta = s.minus(snapshots.removeLast())
     builder.add(delta)
