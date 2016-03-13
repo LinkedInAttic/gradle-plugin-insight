f7fdb6449467c65b1dc18b9c081889f92bd46af4
Szczepan Faber <szczepiq@gmail.com>
2016-03-12 19:17:30 -0800

    Added javadoc

--- src/main/groovy/org/getcraftdone/gradle/api/ProjectProvider.java
+++ src/main/groovy/org/getcraftdone/gradle/api/ProjectProvider.java
@@ -3,8 +3,17 @@ package org.getcraftdone.gradle.api;
 import org.gradle.api.Project;
 
 /**
- * Created by sfaber on 3/8/16.
+ * Provides and instance of a project that we need to apply the plugin, and track changes to the model
+ * and build the documentation.
+ *
+ * Example use case: in my team, instances of Gradle projects require some extra files present and properties present.
+ * Hence, we need to use custom implementation that creates project instances
+ * that can correctly apply our custom plugins.
  */
 public interface ProjectProvider {
+
+    /**
+     * Creates an instance of Gradle project
+     */
     Project buildProject();
 }
